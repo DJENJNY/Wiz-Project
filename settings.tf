9e6386134525eb1d5487feebc7465cbe2e20f40c
Ethan Jones <djenj@MacBook-Pro.local>
2025-09-03 11:15:14 -0500

    updated settings fmt

--- settings.tf
+++ settings.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.3.2"
-  
+
   backend "s3" {
     bucket = "wiz-project"
     key    = "terraform"
@@ -13,11 +13,11 @@ terraform {
       version = "~> 5.99.0"
     }
     cloudinit = {
-      source = "hashicorp/cloudinit"
+      source  = "hashicorp/cloudinit"
       version = "2.3.7"
     }
-    }
   }
+}
 
 
 provider "aws" {
